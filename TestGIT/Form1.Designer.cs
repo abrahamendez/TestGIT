1cedac58a7ec9498427e451cf6a3adcb4f3564c5
abrahamendez <amendez@innovaweb.com.mx>
2013-07-29 11:18:57 -0500

    se cambio de tamaño el formulario 1

--- TestGIT/Form1.Designer.cs
+++ TestGIT/Form1.Designer.cs
@@ -61,7 +61,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(792, 312);
+            this.ClientSize = new System.Drawing.Size(1091, 367);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
